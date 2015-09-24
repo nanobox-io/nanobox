5b989de74ebec7d51ab1cf12acb36ceff1ed2c14
Steve Domino <domino.steve@gmail.com>
2015-09-24 15:53:23 -0600

    engine fetch output to stderr

--- commands/engineFetch.go
+++ commands/engineFetch.go
@@ -21,7 +21,7 @@ import (
 	// "github.com/pagodabox/nanobox-cli/auth"
 	// "github.com/pagodabox/nanobox-cli/config"
 	// "github.com/pagodabox/nanobox-cli/util"
-	// "github.com/pagodabox/nanobox-golang-stylish"
+	"github.com/pagodabox/nanobox-golang-stylish"
 )
 
 //
@@ -57,11 +57,11 @@ func nanoEngineFetch(ccmd *cobra.Command, args []string) {
 	// api.UserSlug, api.AuthToken = auth.Authenticate()
 
 	if len(args) == 0 {
-		// config.Console.Fatal("Please provide the name of an engine you would like to fetch, (run 'nanobox fetch -h' for details)")
+		os.Stderr.WriteString("Please provide the name of an engine you would like to fetch, (run 'nanobox engine fetch -h' for details)")
 		os.Exit(1)
 	}
 
-	// fmt.Printf(stylish.Bullet("Attempting to fetch '%v'", args[0]))
+	os.Stderr.WriteString(stylish.Bullet("Attempting to fetch '%v'", args[0]))
 
 	//
 	var archive, engine, user, version string // various string values used to store pieces of the engine
@@ -112,7 +112,7 @@ func nanoEngineFetch(ccmd *cobra.Command, args []string) {
 	//
 	e, err := api.GetEngine(user, engine)
 	if err != nil {
-		// fmt.Printf(stylish.ErrBullet("No official engine, or engine for that user found."))
+		os.Stderr.WriteString(stylish.ErrBullet("No official engine, or engine for that user found."))
 		os.Exit(1)
 	}
 
@@ -129,12 +129,12 @@ func nanoEngineFetch(ccmd *cobra.Command, args []string) {
 		path = fmt.Sprintf("http://api.nanobox.io/v1/engines/%v/%v/releases/%v/download", user, engine, version)
 	}
 
-	// fmt.Printf(stylish.Bullet("Fetching engine at '%s'", path))
+	os.Stderr.WriteString(stylish.Bullet("Fetching engine at '%s'", path))
 
 	//
 	res, err := http.Get(path)
 	if err != nil {
-		// util.Fatal("[commands.fetch] http.Get() failed", err)
+		panic(err)
 	}
 	defer res.Body.Close()
 
@@ -143,10 +143,10 @@ func nanoEngineFetch(ccmd *cobra.Command, args []string) {
 	case 2, 3:
 		break
 	case 4:
-		// fmt.Printf(stylish.ErrBullet("No release by that version found for engine '%v'", engine))
+		os.Stderr.WriteString(stylish.ErrBullet("No release by that version found for engine '%v'", engine))
 		os.Exit(1)
 	case 5:
-		// fmt.Printf(stylish.ErrBullet("Failed to fetch release (%v).", res.Status))
+		os.Stderr.WriteString(stylish.ErrBullet("Failed to fetch release (%v).", res.Status))
 		os.Exit(1)
 	}
 
@@ -155,17 +155,16 @@ func nanoEngineFetch(ccmd *cobra.Command, args []string) {
 
 	// pipe the ouput to os.Stdout
 	default:
-		// fmt.Printf(stylish.Bullet("Piping download to stdout"))
 		dest = os.Stdout
 
 	// write the download to the local file system
 	case fFile != "":
-		// fmt.Printf(stylish.Bullet("Saving engine as '%s'", fFile))
+		os.Stderr.WriteString(stylish.Bullet("Saving engine as '%s'", fFile))
 
 		//
 		release, err := os.Create(fFile)
 		if err != nil {
-			// fmt.Printf(stylish.ErrBullet("%v", err))
+			os.Stderr.WriteString(stylish.ErrBullet(err.Error()))
 			os.Exit(1)
 		}
 		defer release.Close()
@@ -176,6 +175,6 @@ func nanoEngineFetch(ccmd *cobra.Command, args []string) {
 
 	// write the file
 	if _, err := io.Copy(dest, res.Body); err != nil {
-		// util.Fatal("[commands.fetch] io.Copy() failed", err)
+		os.Stderr.WriteString(fmt.Sprintf("[commands.fetch] io.Copy() failed", err.Error()))
 	}
 }
