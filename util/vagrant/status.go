8714dcc028b4a22d69b477bb1195e4a463814d9d
Steve Domino <domino.steve@gmail.com>
2015-10-13 14:49:15 -0600

    reworked how the status is fetched, to make it super fast (pulled from the machine-index/index file

--- util/vagrant/status.go
+++ util/vagrant/status.go
@@ -9,12 +9,9 @@
 package vagrant
 
 import (
-	"bufio"
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
-	"os/exec"
-	"regexp"
-	"strings"
 
 	"github.com/nanobox-io/nanobox-cli/config"
 )
@@ -24,65 +21,45 @@ import (
 // be available
 func Status() string {
 
-	var status string
-
-	// don't really care about the error here since the value will be "" if there
-	// is no file to read
+	// attempt to get the uuid; don't really care about the error here since the
+	// value will be "" if there is no file to read
 	b, _ := ioutil.ReadFile(fmt.Sprintf("%v/.vagrant/machines/%v/%v/index_uuid", config.AppDir, config.Nanofile.Name, config.Nanofile.Provider))
+
+	// set uuid
 	uuid := string(b)
 
-	// if no uuid is found the vm has not yet been created
-	if uuid == "" {
-		return "not created"
+	// attempt to get the machine data; don't really care about the error here since
+	// the value will be "" if there is no file to read
+	b, _ = ioutil.ReadFile(config.Home + "/.vagrant.d/data/machine-index/index")
+
+	// get the initial data
+	machineIndex := make(map[string]json.RawMessage)
+	if err := json.Unmarshal(b, &machineIndex); err != nil {
+		config.Fatal("[util/vagrant/status] failed - ", err.Error())
 	}
 
-	// run the command with the uuid; this allows the command to be run from any
-	// context (not dependent on a Vagrantfile)
-	cmd := exec.Command("vagrant", "status", uuid)
-
-	//
-	stdout, err := cmd.StdoutPipe()
-	if err != nil {
-		config.Fatal("[util/vagrant] cmd.StdoutPipe() failed", err.Error())
+	// read the machines from machineIndex
+	machines := make(map[string]json.RawMessage)
+	if err := json.Unmarshal(machineIndex["machines"], &machines); err != nil {
+		config.Fatal("[util/vagrant/status] failed - ", err.Error())
 	}
 
-	//
-	scanner := bufio.NewScanner(stdout)
-	go func() {
-		for scanner.Scan() {
-
-			// if the scan line starts with the name of the VM thats the line thats going
-			// to have the status in it
-			if strings.HasPrefix(scanner.Text(), config.Nanofile.Name) {
-
-				// pull the status out of the line; it's easiest to just pull the exact
-				// states out of the string since there aren't that many
-				submatch := regexp.MustCompile(`\s(running|restoring|saved|poweroff|not created|aborted)\s`).FindStringSubmatch(scanner.Text())
-
-				//
-				if len(submatch) > 1 {
-					status = submatch[1]
-				} else {
-					config.Fatal("[util/vagrant] Unknown status: ", scanner.Text())
-				}
-			}
-		}
-	}()
-
-	// start the command; we need this to 'fire and forget' so that we can manually
-	// capture and modify the commands output
-	if err := cmd.Start(); err != nil {
-		config.Fatal("[util/vagrant] cmd.Start() failed", err.Error())
+	// attempt to pull the machine based on the uuid
+	machine := Machine{}
+	if err := json.Unmarshal(machines[uuid], &machine); err != nil {
+		config.Fatal("[util/vagrant/status] failed - ", err.Error())
 	}
 
-	// wait for the command to complete/fail and exit, giving us a chance to scan
-	// the output
-	if err := cmd.Wait(); err != nil {
-		config.Fatal("[util/vagrant] cmd.Wait() failed", err.Error())
-	}
+	// if the uuid is "" or not found in the machines, then the resulting State
+	// will be ""
+	status := machine.State
 
 	// set the status in the .vmfile (currently not used)
 	// config.VMfile.StatusIs(status)
+
+	if machine.State == "" {
+		status = "not created"
+	}
 
 	return status
 }
