e955371ceb02b657d21b2a93c192ba848e405446
Steve Domino <domino.steve@gmail.com>
2015-11-09 13:02:24 -0700

    updating md5 system to use binary instead of local file.

--- commands/update.go
+++ commands/update.go
@@ -5,9 +5,9 @@
 // at http://mozilla.org/MPL/2.0/.
 //
 
+//
 package commands
 
-//
 import (
 	"fmt"
 	"github.com/kardianos/osext"
@@ -33,34 +33,37 @@ var updateCmd = &cobra.Command{
 
 // update
 func update(ccmd *cobra.Command, args []string) {
-	Update()
-	fmt.Printf("Nanobox is now up to date (running v%s)\n", config.VERSION)
+
+	cli, match, err := getUpdateStuff()
+	if err != nil {
+		Config.Fatal("[commands/update] getUpdateStuff() failed", err.Error())
+	}
+
+	// if the md5s don't match or it's been forced, update
+	switch {
+	case config.Force, !match:
+		runUpdate(cli)
+	default:
+		fmt.Printf(stylish.SubBullet("[√] Nanobox is up-to-date"))
+	}
 }
 
 // Update
 func Update() {
 
-	// if there is no nanobox.md5 this is likely the first time nanobox is being
-	// run and it should update
-	if fi, _ := os.Stat(config.Root + "/nanobox.md5"); fi == nil {
-		fmt.Printf(stylish.Bullet("Ensuring nanobox is up to date..."))
-		runUpdate()
-		return
-	}
-
-	//
-	match, err := Util.MD5sMatch(config.Root+"/nanobox.md5", "https://s3.amazonaws.com/tools.nanobox.io/cli/nanobox.md5")
+	cli, match, err := getUpdateStuff()
 	if err != nil {
-		Config.Fatal("[commands/update] util.MD5sMatch() failed", err.Error())
+		Config.Fatal("[commands/update] getUpdateStuff() failed", err.Error())
 	}
 
-	// an error here just means the file doesn't exist (which should never happen
-	// since it gets created in the config init at startup)
+	// stat the update file to get ModTime(); an error here means the file doesn't
+	// exist. This is highly unlikely as the file is created if it doesn't exist
+	// each time the CLI is run.
 	fi, _ := os.Stat(config.UpdateFile)
 
-	// if the last update was longer ago than our wait time (14 days), and the md5s
-	// dont match, then update
-	if time.Since(fi.ModTime()).Hours() >= 336.0 && !match {
+	// if the md5s don't match and it's 'time' for an update (14 days), OR a force
+	// update is issued, update
+	if !match && time.Since(fi.ModTime()).Hours() >= 336.0 {
 
 		//
 		switch printutil.Prompt("Nanobox is out of date, would you like to update it now (y/N)? ") {
@@ -72,42 +75,37 @@ func Update() {
 
 		// if yes continue to update
 		case "Yes", "yes", "Y", "y":
-			runUpdate()
+			runUpdate(cli)
 		}
 	}
 }
 
 // runUpdate
-func runUpdate() {
+func runUpdate(path string) {
 
 	fmt.Printf(stylish.Bullet("Updating nanobox..."))
 
-	// get the path of the current executing CLI
-	path, err := osext.Executable()
-	if err != nil {
-		Config.Fatal("[commands/update] osext.ExecutableFolder() failed", err.Error())
-	}
-
-	// download the CLI
+	// create a new CLI at the location of the old one
 	cli, err := os.Create(path)
 	if err != nil {
 		Config.Fatal("[commands/update] os.Create() failed", err.Error())
 	}
 	defer cli.Close()
 
-	//
+	// download the new cli
 	fileutil.Progress(fmt.Sprintf("https://s3.amazonaws.com/tools.nanobox.io/cli/%v/%v/nanobox", runtime.GOOS, runtime.GOARCH), cli)
 
-	//
-	// download the CLI md5
-	md5, err := os.Create(config.Root + "/nanobox.md5")
+	// ensure the newly downloaded cli matches the remote
+	match, err := Util.MD5sMatch(path, "https://s3.amazonaws.com/tools.nanobox.io/cli/nanobox.md5")
 	if err != nil {
-		Config.Fatal("[commands/update] os.Create() failed", err.Error())
+		Config.Fatal("[commands/update] util.MD5sMatch() failed", err.Error())
 	}
-	defer md5.Close()
 
-	//
-	fileutil.Download("https://s3.amazonaws.com/tools.nanobox.io/cli/nanobox.md5", md5)
+	// if they don't match it's the wrong CLI
+	if !match {
+		fmt.Println("MD5 checksum failed! Your nanobox-desktop (CLI) is not ours!")
+		return
+	}
 
 	// if the new CLI fails to execute, just print a generic message and return
 	out, err := exec.Command(path, "-v").Output()
@@ -122,4 +120,20 @@ func runUpdate() {
 	if err := os.Chtimes(config.UpdateFile, time.Now(), time.Now()); err != nil {
 		Config.Fatal("[commands.update] os.Chtimes() failed", err.Error())
 	}
+}
+
+// getUpdateStuff
+func getUpdateStuff() (cli string, match bool, err error) {
+
+	// get the path of the current executing CLI
+	if cli, err = osext.Executable(); err != nil {
+		return
+	}
+
+	// check the current cli md5 against the remote md5
+	if match, err = Util.MD5sMatch(cli, "https://s3.amazonaws.com/tools.nanobox.io/cli/nanobox.md5"); err != nil {
+		return
+	}
+
+	return
 }
