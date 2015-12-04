eb208f5912e9e07246699cf0f2725f1eb3522a92
Lyon Hill <lyondhill@gmail.com>
2015-12-04 11:25:27 -0700

    fix a timeout reader typo in the server test

--- util/server/handlers_internal_test.go
+++ util/server/handlers_internal_test.go
@@ -8,28 +8,28 @@ import (
 
 func TestTimeoutReader(t *testing.T) {
 	// create a new timeout reader
-	timoutReader = &TimeoutReader{
+	timeoutReader = &TimeoutReader{
 		Files:   make(chan string, 1),
 		timeout: time.Second,
 	}
 
 	bytes := make([]byte, 4)
 
-	timoutReader.Files <- "1"
-	n, err := timoutReader.Read(bytes)
+	timeoutReader.Files <- "1"
+	n, err := timeoutReader.Read(bytes)
 	if n != 2 || err != nil || string(bytes[:n]) != "1\n" {
 		t.Errorf("the reader didnt output the right data n %d, err %+v, data %q", n, err, bytes)
 	}
 
-	timoutReader.Files <- "123"
-	n, err = timoutReader.Read(bytes)
+	timeoutReader.Files <- "123"
+	n, err = timeoutReader.Read(bytes)
 	if n != 4 || err != nil || string(bytes[:n]) != "123\n" {
 		t.Errorf("the reader didnt output the right data n %d, err %+v, data %q", n, err, bytes)
 	}
 
 	// now attempt a read without writing
 	// should timeout
-	n, err = timoutReader.Read(bytes)
+	n, err = timeoutReader.Read(bytes)
 	if n != 0 || err != io.EOF {
 		t.Errorf("the timeout reader didnt timeout")
 	}
