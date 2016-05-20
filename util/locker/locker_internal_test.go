680cbacbe055272717795797bfda357c04377d47
Lyon Hill <lyondhill@gmail.com>
2016-05-20 11:11:20 -0600

    update the testing and travis files to see if i can get travis to succseed

--- util/locker/locker_internal_test.go
+++ util/locker/locker_internal_test.go
@@ -2,35 +2,113 @@ package locker
 
 import "testing"
 
-func TestLock(t *testing.T) {
-	err := Lock()
+func TestLocalLock(t *testing.T) {
+	err := LocalLock()
 	if err != nil {
 		t.Errorf("unable to aquire lock %+v", err)
 	}
-	if ln == nil {
+	if lln == nil {
 		t.Errorf("lock was aqquired but the listener is not populated")
 	}
 }
 
-func TestTryLock(t *testing.T) {
-	locked, err := TryLock()
+func TestLocalTryLock(t *testing.T) {
+	locked, err := LocalTryLock()
 	if err != nil {
 		t.Errorf("lock check failed %+v", err)
 	}
 	if !locked {
 		t.Errorf("lock was not aqquired")
 	}
-	if ln == nil {
+	if lln == nil {
 		t.Errorf("lock was aqquired but the listener is not populated")
 	}
 }
 
-func TestUnlock(t *testing.T) {
-	err := Unlock()
+func TestLocalUnlock(t *testing.T) {
+	err := LocalUnlock()
 	if err != nil {
 		t.Errorf("unlock failed %+v", err)
 	}
-	if ln != nil {
+	if lln != nil {
 		t.Errorf("unlock succseeded but the listener is still present")
 	}
 }
+
+func TestLocalStackLocking(t *testing.T) {
+	for i := 0; i < 10; i++ {
+		if lCount != i {
+			t.Errorf("global count not equil to lock calls(%d)", gCount)
+		}
+		err := LocalLock()
+		if err != nil {
+			t.Errorf("errored on multiple locks")
+		}
+	}
+	for i := 10; i > 0; i-- {
+		if lCount != i {
+			t.Errorf("global count not equil to lock calls(%d)", gCount)
+		}
+		err := LocalUnlock()
+		if err != nil {
+			t.Errorf("errored on multiple locks")
+		}
+	}	
+}
+
+func TestGlobalLock(t *testing.T) {
+	err := GlobalLock()
+	if err != nil {
+		t.Errorf("unable to aquire lock %+v", err)
+	}
+	if gln == nil {
+		t.Errorf("lock was aqquired but the listener is not populated")
+	}
+}
+
+func TestGlobalTryLock(t *testing.T) {
+	locked, err := GlobalTryLock()
+	if err != nil {
+		t.Errorf("lock check failed %+v", err)
+	}
+	if !locked {
+		t.Errorf("lock was not aqquired")
+	}
+	if gln == nil {
+		t.Errorf("lock was aqquired but the listener is not populated")
+	}
+}
+
+func TestGlobalUnlock(t *testing.T) {
+	err := GlobalUnlock()
+	if err != nil {
+		t.Errorf("unlock failed %+v", err)
+	}
+	if gln != nil {
+		t.Errorf("unlock succseeded but the listener is still present")
+	}
+}
+
+func TestGlobalStackLocking(t *testing.T) {
+	for i := 0; i < 10; i++ {
+		if gCount != i {
+			t.Errorf("global count not equil to lock calls(%d)", gCount)
+		}
+		err := GlobalLock()
+		if err != nil {
+			t.Errorf("errored on multiple locks")
+		}
+	}
+	for i := 10; i > 0; i-- {
+		if gCount != i {
+			t.Errorf("global count not equil to lock calls(%d)", gCount)
+		}
+		err := GlobalUnlock()
+		if err != nil {
+			t.Errorf("errored on multiple locks")
+		}
+	}	
+}
+
+
+
