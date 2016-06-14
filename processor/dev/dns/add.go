a54cc6181b05de5d061211e7a1468a13696cd1ff
Tyler Flint <tylerflint@gmail.com>
2016-06-14 16:00:00 -0600

    Restructure the process function to accomodate a flow purpose

--- processor/dev/dns/add.go
+++ processor/dev/dns/add.go
@@ -15,6 +15,7 @@ import (
 // processDevDNSAdd ...
 type processDevDNSAdd struct {
 	control processor.ProcessControl
+	app			models.App
 }
 
 //
@@ -35,42 +36,81 @@ func (devDNSAdd processDevDNSAdd) Results() processor.ProcessControl {
 //
 func (devDNSAdd processDevDNSAdd) Process() error {
 
-	//
-	name := devDNSAdd.control.Meta["name"]
+	if err := devDNSAdd.validateMeta(); err != nil {
+		return err
+	}
 
-	//
-	app := models.App{}
-	data.Get("apps", config.AppName(), &app)
+	if err := devDNSAdd.loadApp(); err != nil {
+		return err
+	}
 
-	//
-	preview := dns.Entry(app.DevIP, name, "preview")
-	dev := dns.Entry(app.DevIP, name, "dev")
-
-	// if the entry doesnt exist just return
-	if !dns.Exists(preview) && !dns.Exists(dev) {
+	// short-circuit if the entries already exist
+	if devDNSAdd.entriesExist() {
 		return nil
 	}
 
-	// This process requires root, check to see if we're the root user. If not, we
-	// need to run a hidden command as sudo that will just call this function again.
-	// Thus, the subprocess will be running as root
-	if os.Geteuid() != 0 {
+	// if we're not running as the privileged user, we need to re-exec with privilege
+	if !util.IsPrivileged() {
 
-		// get the original nanobox executable
-		nanobox := os.Args[0]
-
-		// call 'dev dns add' with the original path (ultimately leads right back here)
-		cmd := fmt.Sprintf("%s dev dns add %s", nanobox, name)
-
-		// if the sudo'ed subprocess fails, we need to return error to stop the process
-		fmt.Println("Admin privileges are required to add DNS entries to your hosts file, your password may be requested...")
-		if err := util.PrivilegeExec(cmd); err != nil {
+		if err := devDNSAdd.reExecPrivilege(); err != nil {
 			return err
 		}
 
-		// the subprocess exited successfully, so we can short-circuit here
 		return nil
 	}
+
+	if err := devDNSAdd.addEntries(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// validateMeta validates that the required metadata exists
+func (devDNSAdd processDevDNSAdd) validateMeta() error {
+
+	// ensure name is provided
+	if devDNSAdd.control.Meta["name"] == "" {
+		return errors.New("Name is required")
+	}
+
+	return nil
+}
+
+// loadApp loads the app from the db
+func (devDNSAdd processDevDNSAdd) loadApp() error {
+
+	if err := data.Get("apps", config.AppName(), &devDNSAdd.app); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// entriesExist returns true if both entries already exist
+func (devDNSAdd processDevDNSAdd) entriesExist() bool {
+
+	name := devDNSAdd.control.Meta["name"]
+
+	// generate the entries
+	preview := dns.Entry(devDNSAdd.app.DevIP, name, "preview")
+	dev := dns.Entry(devDNSAdd.app.DevIP, name, "dev")
+
+	// if the entry doesnt exist just return
+	if dns.Exists(preview) && dns.Exists(dev) {
+		return true
+	}
+
+	return false
+}
+
+// addEntries adds the dev and preview entries into the host dns
+func (devDNSAdd processDevDNSAdd) addEntries() error {
+	name := devDNSAdd.control.Meta["name"]
+
+	// generate the entries
+	preview := dns.Entry(devDNSAdd.app.DevIP, name, "preview")
+	dev := dns.Entry(devDNSAdd.app.DevIP, name, "dev")
 
 	// add the 'preview' DNS entry into the /etc/hosts file
 	if err := dns.Add(preview); err != nil {
@@ -79,6 +119,25 @@ func (devDNSAdd processDevDNSAdd) Process() error {
 
 	// add the 'dev' DNS entry into the /etc/hosts file
 	if err := dns.Add(dev); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// reExecPrivilege re-execs the current process with a privileged user
+func (devDNSAdd processDevDNSAdd) reExecPrivilege() error {
+	name := devDNSAdd.control.Meta["name"]
+
+	// get the original nanobox executable
+	nanobox := os.Args[0]
+
+	// call 'dev dns add' with the original path (ultimately leads right back here)
+	cmd := fmt.Sprintf("%s dev dns add %s", nanobox, name)
+
+	// if the sudo'ed subprocess fails, we need to return error to stop the process
+	fmt.Println("Admin privileges are required to add DNS entries to your hosts file, your password may be requested...")
+	if err := util.PrivilegeExec(cmd); err != nil {
 		return err
 	}
 
--- util/util_unix.go
+++ util/util_unix.go
@@ -8,6 +8,19 @@ import (
 	"os/exec"
 )
 
+// IsPrivileged will return true if the current process is running under
+// a privileged user, like root
+func IsPrivileged() bool {
+
+	// Execute a syscall to return the user id. If the user id is 0
+	// then we're running with root escalation.
+	if os.Geteuid() != 0 {
+		return true
+	}
+
+	return false
+}
+
 // PrivilegeExec runs a command as sudo
 func PrivilegeExec(command string) error {
 	//
