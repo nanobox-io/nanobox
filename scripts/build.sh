dc71be3885ecb6edc8a9fa3658b25a854a89f703
Tyler Flint <tylerflint@gmail.com>
2016-08-22 10:48:48 -0600

    Change build to a hidden directory

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,16 +2,16 @@
 set -e
 
 # remove any previous builds that may have failed
-[ -e "./build" ] && \
+[ -e "./.build" ] && \
   echo "Cleaning up old builds..." && \
-  rm -rf "./build"
+  rm -rf "./.build"
 
 printf "\nBuilding nanobox...\n"
 
 # build nanobox
-gox -osarch "darwin/amd64 linux/amd64 windows/amd64" -output="./build/v1/{{.OS}}/{{.Arch}}/nanobox"
+gox -osarch "darwin/amd64 linux/amd64 windows/amd64" -output="./.build/v1/{{.OS}}/{{.Arch}}/nanobox"
 
 printf "\nBuilding nanobox updater...\n"
 
 # change into updater directory and build nanobox updater
-cd ./updater && gox -osarch "darwin/amd64 linux/amd64 windows/amd64" -output="../build/v1/{{.OS}}/{{.Arch}}/nanobox-update"
+cd ./updater && gox -osarch "darwin/amd64 linux/amd64 windows/amd64" -output="../.build/v1/{{.OS}}/{{.Arch}}/nanobox-update"
--- scripts/upload.sh
+++ scripts/upload.sh
@@ -7,14 +7,14 @@ MD5=$(which md5 || echo "$(which md5sum) | cut -f 1" )
 echo "Generating md5s..."
 
 # look through each os/arch/file and generate an md5 for each
-for os in $(ls ./build/v1); do
-  for arch in $(ls ./build/v1/${os}); do
-    for file in $(ls ./build/v1/${os}/${arch}); do
-      cat "./build/v1/${os}/${arch}/${file}" | ${MD5} >> "./build/v1/${os}/${arch}/${file}.md5"
+for os in $(ls ./.build/v1); do
+  for arch in $(ls ./.build/v1/${os}); do
+    for file in $(ls ./.build/v1/${os}/${arch}); do
+      cat "./.build/v1/${os}/${arch}/${file}" | ${MD5} >> "./.build/v1/${os}/${arch}/${file}.md5"
     done
   done
 done
 
 # upload to AWS S3
 echo "Uploading builds to S3..."
-aws s3 sync ./build/v1/ s3://tools.nanobox.io/nanobox/v1 --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers --region us-east-1
+aws s3 sync ./.build/v1/ s3://tools.nanobox.io/nanobox/v1 --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers --region us-east-1
