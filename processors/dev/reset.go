77848ec9d4c27f0e72d33ae4912997ca3f09f029
Tyler Flint <tylerflint@gmail.com>
2016-09-02 10:48:21 -0600

    Add display logic to dev reset processor

--- processors/dev/reset.go
+++ processors/dev/reset.go
@@ -1,10 +1,23 @@
 package dev
 
 import (
+	"fmt"
+	
 	"github.com/nanobox-io/nanobox/models"
+	"github.com/nanobox-io/nanobox/util/display"
 )
 
 // Reset all the counters for all dev applications
 func Reset() error {
-	return models.DeleteAllCounters()
+	display.OpenContext("Resetting dev state")
+	defer display.CloseContext()
+	
+	display.StartTask("Clear usage counters")
+	if err := models.DeleteAllCounters(); err != nil {
+		display.ErrorTask()
+		return fmt.Errorf("failed to remove usage counters: %s", err.Error())
+	}
+	display.StopTask()
+	
+	return nil
 }
