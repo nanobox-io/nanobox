57f63ae5cdf345f1d525180559aa50a7797af899
Tyler Flint <tylerflint@gmail.com>
2016-09-05 15:16:20 -0600

    Start to layer in link formatting

--- processors/link/add.go
+++ processors/link/add.go
@@ -5,12 +5,13 @@ import (
 
 	"github.com/nanobox-io/nanobox/models"
 	"github.com/nanobox-io/nanobox/util/config"
+	"github.com/nanobox-io/nanobox/util/display"
 	"github.com/nanobox-io/nanobox/util/odin"
 )
 
-// Add
 func Add(envModel *models.Env, appName, alias string) error {
 
+	// ensure the env model has been generated
 	if err := envModel.Generate(); err != nil {
 		return fmt.Errorf("unable to generate the environment")
 	}
@@ -20,22 +21,34 @@ func Add(envModel *models.Env, appName, alias string) error {
 		alias = "default"
 	}
 
-	// set the appName to the folder name its missing
+	// set the appName to the folder name if its missing
 	if appName == "" {
 		appName = config.LocalDirName()
 	}
 
-	// get app id
+	// fetch the odin app
 	app, err := odin.App(appName)
 	if err != nil {
-		return err
+		fmt.Printf("! Sorry, but you don't have access to %s\n", appName)
+		return nil
 	}
 
+	// ensure the links map is initialized
 	if envModel.Links == nil {
 		envModel.Links = map[string]string{}
 	}
 
 	envModel.Links[alias] = app.ID
 
-	return envModel.Save()
+	if err := envModel.Save(); err != nil {
+		return fmt.Errorf("failed to save link: %s", err.Error())
+	}
+
+	fmt.Printf("%s Codebase linked to %s\n", display.TaskComplete, appName)
+	
+	if alias != "default" {
+		fmt.Printf("  through the '%s' alias\n", alias)
+	}
+
+	return nil
 }
--- processors/link/list.go
+++ processors/link/list.go
@@ -2,14 +2,62 @@ package link
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/nanobox-io/nanobox/models"
 )
 
 // List ...
-func List(envModel *models.Env) error {
+func List(env *models.Env) error {
 
-	fmt.Printf("%+v\n", envModel.Links)
+	// set the left column width to the longest name
+	leftColWidth := len(longestName(env)) + 2
+	
+	// unless the longest name is less than 10 characters :)
+	if leftColWidth < 10 {
+		leftColWidth = 10
+	}
+
+	// print the header
+	margin := strings.Repeat(" ", leftColWidth - 8)
+	fmt.Printf("\nApp Name%s: Alias\n", margin)
+	separater := strings.Repeat("-", leftColWidth + len(longestAlias(env)) + 1)
+	fmt.Printf("%s\n", separater)
+	
+	// print the table
+	for name, alias := range env.Links {
+		margin := strings.Repeat(" ", leftColWidth - len(name))
+		fmt.Printf("%s%s: %s\n", name, margin, alias)
+	}
+
+	// end with a newline
+	fmt.Println()
 
 	return nil
+}
+
+// returns the longest name
+func longestName(env *models.Env) string {
+  longest := ""
+  
+  for name, _ := range env.Links {
+    if len(name) > len(longest) {
+      longest = name
+    }
+  }
+  
+  return longest
+}
+
+// returns the longest alias
+func longestAlias(env *models.Env) string {
+	longest := ""
+	
+	for _, alias := range env.Links {
+		if len(alias) > len(longest) {
+			longest = alias
+		}
+	}
+	
+	return longest
 }
