29221fe40af4a0ce3ee1822c230a5dfeacabf480
Tyler Flint <tylerflint@gmail.com>
2016-09-05 10:19:14 -0600

    Adding display logic to clean process

--- processors/clean.go
+++ processors/clean.go
@@ -6,6 +6,7 @@ import (
 	"github.com/nanobox-io/nanobox/models"
 	"github.com/nanobox-io/nanobox/processors/env"
 	"github.com/nanobox-io/nanobox/util"
+	"github.com/nanobox-io/nanobox/util/display"
 	"github.com/nanobox-io/nanobox/util/locker"
 )
 
@@ -13,6 +14,12 @@ import (
 func Clean(envModels []*models.Env) error {
 	locker.GlobalLock()
 	defer locker.GlobalUnlock()
+
+	display.OpenContext("Cleaning stale environments")
+	defer display.CloseContext()
+
+	// if any of the apps are stale, we'll mark this to true
+	stale := false
 
 	for _, envModel := range envModels {
 		// check to see if the app folder still exists
@@ -22,6 +29,11 @@ func Clean(envModels []*models.Env) error {
 				return fmt.Errorf("unable to destroy environment(%s): %s", envModel.Name, err)
 			}
 		}
+	}
+	
+	if !stale {
+		display.StartTask("Skipping (none detected)")
+		display.StopTask()
 	}
 
 	return nil
