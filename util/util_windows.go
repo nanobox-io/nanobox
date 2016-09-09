946873bd24eacca5c244e58668880386a8af1f5a
Tyler Flint <tylerflint@gmail.com>
2016-09-09 16:50:50 -0600

    Add custom password prompt for windows

--- util/util_windows.go
+++ util/util_windows.go
@@ -84,13 +84,33 @@ func PowerShell(command string) ([]byte, error) {
 	return cmd.CombinedOutput()
 }
 
-// TODO: write a windows version that squashes the warning (tyler knows)
+// ReadPassword reads a password from the terminal and masks the input
 func ReadPassword() (string, error) {
-	fmt.Print("Password: ")
-	pass, err := terminal.ReadPassword(int(os.Stdin.Fd()))
-	fmt.Println("")
+	
+	// Fetch the current state of the terminal so it can be restored later
+	oldState, err := terminal.GetState(int(os.Stdin.Fd()))
+	if err != nil {
+		return "", err
+	}
+	// Turn off echo and make stdin blank
+	terminal.MakeRaw(int(os.Stdin.Fd()))
+	// Restore echo after the function exits
+	defer terminal.Restore(int(os.Stdin.Fd()), oldState)
+	
+	fmt.Printf("Password: ")
 
-	return string(pass), err
+	// Read the password from stdin
+	t := terminal.NewTerminal(os.Stdin, "")
+	pass, err := t.ReadPassword("")
+	
+	// Add a newline so the next output isn't next to the Password: 
+	fmt.Println("")
+	
+	if err != nil {
+		return "", err
+	}
+	
+	return pass, nil
 }
 
 // extracts the executable from the args
