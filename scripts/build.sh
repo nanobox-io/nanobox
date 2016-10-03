ec526e8db438b2be8259904fc1bcc95a0f1843a2
Greg Linton <lintong@pagodabox.com>
2016-10-03 16:32:41 -0600

    Add version command

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,16 @@
 #!/usr/bin/env bash
 set -e
 
+# for versioning
+getCurrCommit() {
+  echo `git rev-parse --short HEAD | tr -d "[ \r\n\']"`
+}
+
+# for versioning
+getCurrTag() {
+  echo `git describe --always --tags --abbrev=0 | tr -d "[v\r\n]"`
+}
+
 # remove any previous builds that may have failed
 [ -e "./.build" ] && \
   echo "Cleaning up old builds..." && \
@@ -9,7 +19,12 @@ set -e
 printf "\nBuilding nanobox...\n"
 
 # build nanobox
-gox -ldflags "-X main.bugsnagToken=$BUGSNAG_TOKEN -X github.com/nanobox-io/nanobox/util/mixpanel.token=$MIXPANEL_TOKEN" -osarch "darwin/amd64 linux/amd64 windows/amd64" -output="./.build/v1/{{.OS}}/{{.Arch}}/nanobox"
+gox -ldflags "-X main.bugsnagToken=$BUGSNAG_TOKEN
+	-X github.com/nanobox-io/nanobox/commands.tag=$(getCurrTag)
+	-X github.com/nanobox-io/nanobox/commands.commit=$(getCurrCommit)
+	-X github.com/nanobox-io/nanobox/util/mixpanel.token=$MIXPANEL_TOKEN" \
+	-osarch "darwin/amd64 linux/amd64 windows/amd64" \
+	-output="./.build/v1/{{.OS}}/{{.Arch}}/nanobox"
 
 printf "\nBuilding nanobox updater...\n"
 
