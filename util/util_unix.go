ce3bace06f48eecad0a16e6a5b34628856313383
Lyon Hill <lyondhill@gmail.com>
2016-11-03 11:25:09 -0600

    add an acception for when sudo isnt present fixes #241

--- util/util_unix.go
+++ util/util_unix.go
@@ -3,6 +3,7 @@
 package util
 
 import (
+	"bufio"
 	"fmt"
 	"os"
 	"os/exec"
@@ -24,6 +25,15 @@ func IsPrivileged() bool {
 // PrivilegeExec runs a command as sudo
 func PrivilegeExec(command string) error {
 	//
+	if !sudoExists() {
+		fmt.Println("We could not find the 'sudo' in your path")
+		fmt.Println("please run the following command then press enter when its complete")
+		fmt.Printf("sudo %v --internal", command)
+		reader := bufio.NewReader(os.Stdin)
+		reader.ReadString('\n')
+		return nil
+	}
+
 	cmd := exec.Command("/bin/sh", "-c", fmt.Sprintf("sudo %v --internal", command))
 
 	cmd.Stdin = os.Stdin
@@ -32,4 +42,9 @@ func PrivilegeExec(command string) error {
 
 	// run command
 	return cmd.Run()
+}
+
+func sudoExists() bool {
+	_, err := exec.LookPath("sudo")
+	return err == nil
 }
