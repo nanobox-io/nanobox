f6ceab1429eacb282bf128703dd2cb952f2a3dd4
Lyon Hill <lyondhill@gmail.com>
2017-01-20 11:20:03 -0700

    update the command structure of nanobox configure

--- commands/configure.go
+++ commands/configure.go
@@ -23,29 +23,79 @@ Walks through a series of question prompts that modify your local
 Nanobox configuration (~/.nanobox/config.yml).
 		`,
 		Run: configureFn,
+		Aliases: []string{"config"},
+	}
+
+	ConfigureSetCmd = &cobra.Command{
+		Use:   "set",
+		Short: "Set a configuration key",
+		Long: `
+Set a key in the configuration		
+		`,
+		Run: configureSetFn,
+	}
+
+	ConfigureGetCmd = &cobra.Command{
+		Use:   "get",
+		Short: "Get a value form the configuration",
+		Long: `
+Get a key from the configuration
+		`,
+		Run: configureGetFn,
+	}
+
+	ConfigureListCmd = &cobra.Command{
+		Use:   "list",
+		Short: "Show the full configuration",
+		Long: `
+List the full configuration.
+		`,
+		Run: configureListFn,
 	}
 )
 
 func init() {
 	steps.Build("configure", configureComplete, configureFn)
+
+	ConfigureCmd.AddCommand(ConfigureSetCmd)
+	ConfigureCmd.AddCommand(ConfigureGetCmd)
+	ConfigureCmd.AddCommand(ConfigureListCmd)
+
 }
 
 // configureFn ...
 func configureFn(ccmd *cobra.Command, args []string) {
-	// if they want to configure a key/value
-	// show the config
-	if len(args) == 1 {
-		config, _ := models.LoadConfig()
-		prettyJson, _ := json.MarshalIndent(config, "", "  ")
-		fmt.Printf("%s\n", prettyJson)
-		return
 
-	}
-	if len(args) == 2 {
-		display.CommandErr(processors.ConfigureSet(args[0], args[1]))
+	display.CommandErr(processors.Configure())
+}
+
+func configureSetFn(ccmd *cobra.Command, args []string) {
+	if len(args) != 2 {
+		fmt.Println("setting a key requires <key> <value>")
+		return
+	}	
+	display.CommandErr(processors.ConfigureSet(args[0], args[1]))
+}
+
+func configureGetFn(ccmd *cobra.Command, args []string) {
+	if len(args) != 1 {
+		fmt.Println("what is the key you would like to see")
 		return
 	}
-	display.CommandErr(processors.Configure())
+	config, _ := models.LoadConfig()
+	jsonData, _ := json.Marshal(config)
+	configMap := map[string]interface{}{}
+	json.Unmarshal(jsonData, &configMap)
+	fmt.Println(configMap[args[0]])
+	return	
+
+}
+
+func configureListFn(ccmd *cobra.Command, args []string) {
+	config, _ := models.LoadConfig()
+	prettyJson, _ := json.MarshalIndent(config, "", "  ")
+	fmt.Printf("%s\n", prettyJson)
+	return
 }
 
 func configureComplete() bool {
