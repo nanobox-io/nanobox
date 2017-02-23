0436fdfbc52d83b369a3917aa495bb41e2c8c066
Lyon Hill <lyondhill@gmail.com>
2017-02-23 13:54:57 -0700

    update the version and changelog to 2.0.3

--- main.go
+++ main.go
@@ -116,7 +116,7 @@ func setupBugsnag() {
 		APIKey:       bugsnagToken,
 		Logger:       bugLog{},
 		Synchronous:  true,
-		AppVersion:   "2.0.2",
+		AppVersion:   "2.0.3",
 		PanicHandler: func() {}, // the built in panic handler reexicutes our code
 	})
 
