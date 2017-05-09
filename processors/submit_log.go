49586b4fcb3b9db2b5104fb194533bdef38bcab9
Lyon Hill <lyondhill@gmail.com>
2017-05-09 12:34:09 -0600

    dont submit logs when config is set

--- processors/submit_log.go
+++ processors/submit_log.go
@@ -20,7 +20,10 @@ func SubmitLog(args string) error {
 
 	auth, _ := models.LoadAuth()
 	conf, _ := models.LoadConfig()
-	if conf.CIMode {
+	
+	// if we are in ci mode or we are setting a configuration
+	// leave here
+	if strings.Contains(args, "config") || conf.CIMode {
 		return nil
 	}
 
@@ -35,7 +38,7 @@ func SubmitLog(args string) error {
 	app := ""
 
 	env, err := models.FindEnvByID(config.EnvID())
-	if strings.Contains(args, "deploy") || strings.Contains(args, "tunnel") || strings.Contains(args, "console") {
+	if (strings.Contains(args, "deploy") || strings.Contains(args, "tunnel") || strings.Contains(args, "console")) {
 		if err == nil {
 			remote, ok := env.Remotes["default"]
 			if ok {
