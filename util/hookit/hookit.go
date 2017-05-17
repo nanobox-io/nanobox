c159a269fd733408669eefe52db2bd94d15a50d3
Lyon <lyondhill@gmail.com>
2017-05-17 14:52:53 -0600

    update hookit debug to show more detail

--- util/hookit/hookit.go
+++ util/hookit/hookit.go
@@ -3,6 +3,7 @@ package hookit
 import (
 	"fmt"
 	"strings"
+	"io"
 
 	"github.com/nanobox-io/nanobox/commands/registry"
 	"github.com/nanobox-io/nanobox/util"
@@ -10,9 +11,18 @@ import (
 	"github.com/nanobox-io/nanobox/util/display"
 )
 
+var combined bool
+
 // Exec executes a hook inside of a container
 func Exec(container, hook, payload, displayLevel string) (string, error) {
-	out, err := util.DockerExec(container, "root", "/opt/nanobox/hooks/"+hook, []string{payload}, display.NewStreamer(displayLevel))
+
+	var stream io.Writer
+
+	if !combined {
+		stream = display.NewStreamer(displayLevel)
+	}
+
+	out, err := util.DockerExec(container, "root", "/opt/nanobox/hooks/"+hook, []string{payload}, stream)
 	if err != nil && (strings.Contains(string(out), "such file or directory") && strings.Contains(err.Error(), "bad exit code(126)")) {
 		// if its a 126 the hook didnt exist
 		return "", nil
@@ -31,10 +41,14 @@ func DebugExec(container, hook, payload, displayLevel string) (string, error) {
 	if err == nil {
 		return res, err
 	}
-
 	display.ErrorTask()
-	err = fmt.Errorf("failed to execute %s hook: %s", hook, err.Error())
+
+	combined = true
+	res, err = Exec(container, hook, payload, displayLevel)
+
+	fmt.Printf("failed to execute %s hook: %s\n", hook, err.Error())
 	if registry.GetBool("debug") {
+
 		fmt.Printf("An error has occurred: \"%s\"\n", res)
 		fmt.Println("Entering Debug Mode")
 		fmt.Printf("  container: %s\n", container)
