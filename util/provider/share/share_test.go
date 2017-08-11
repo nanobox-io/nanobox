a90cce7bf7f2882beeb4eceda6cc4cb938a39346
Greg Linton <lintong@pagodabox.com>
2017-08-11 13:05:32 -0600

    Update tests for removed functionality
    If no boxfile.yml was present, nanobox would traverse up the directory tree looking for a boxfile and use that to build and would set that as the mountpoint.
    Turns out we still want that behavior so I'll put it in somewhere else maybe.

--- util/provider/share/share_test.go
+++ util/provider/share/share_test.go
@@ -22,42 +22,60 @@ func TestMain(m *testing.M) {
 	provider.Save()
 
 	share.EXPORTSFILE = "/tmp/exports"
+	os.Remove(share.EXPORTSFILE)
 	exitCode := m.Run()
-	os.Remove("/tmp/exports")
+	os.Remove(share.EXPORTSFILE)
 	provider.Delete()
 	os.Exit(exitCode)
 }
 
+// TestShare tests adding parent first, so we can test if removal of parent removes child/sub path
 func TestShare(t *testing.T) {
-
+	// path = /go/src/github.com/nanobox-io/nanobox/util/provider/share
 	path := config.LocalDir()
+	// parentPath = /go/src/github.com/nanobox-io/nanobox/util/provider
+	parentPath := filepath.Dir(path)
+	if share.Exists(parentPath) {
+		t.Fatal("parent appears to exist when it shouldnt")
+	}
+
+	// add parent path to /tmp/exports
+	if err := share.Add(parentPath); err != nil {
+		t.Fatal("error adding parent", err)
+	}
+
+	if !share.Exists(parentPath) {
+		t.Fatal("parent didnt exist")
+	}
+
 	if share.Exists(path) {
 		t.Errorf("the path shouldnt exist but it did")
 	}
 
+	// add path to /tmp/exports
 	if err := share.Add(path); err != nil {
 		t.Fatal("error adding", err)
+	}
+
+	if !share.Exists(parentPath) {
+		t.Fatal("parentPath should still exist")
 	}
 
 	if !share.Exists(path) {
 		t.Fatal("the path didnt exist when it should")
 	}
 
-	subPath := filepath.Join(path, "util")
-	if share.Exists(subPath) {
-		t.Fatal("subpath appears to exist when it shouldnt")
-	}
-
-	if err := share.Add(subPath); err != nil {
-		t.Fatal("error adding subpath", err)
-	}
-
-	if !share.Exists(subPath) {
-		t.Fatal("subpath didnt exist")
+	// remove shares from /tmp/exports
+	if err := share.Remove(parentPath); err != nil {
+		t.Errorf("failed to remove the parentPath %s", err)
 	}
 
 	if !share.Exists(path) {
-		t.Fatal("path didnt exist")
+		t.Errorf("child path should exist")
+	}
+
+	if share.Exists(parentPath) {
+		t.Errorf("parent exists after it shouldnt")
 	}
 
 	if err := share.Remove(path); err != nil {
@@ -67,21 +85,4 @@ func TestShare(t *testing.T) {
 	if share.Exists(path) {
 		t.Errorf("failed to remove path")
 	}
-
-	if !share.Exists(subPath) {
-		t.Errorf("removing path also removed subpath")
-	}
-
-	if err := share.Remove(subPath); err != nil {
-		t.Errorf("failed to remove the subPath %s", err)
-	}
-
-	if share.Exists(path) {
-		t.Errorf("path exists after it shouldnt")
-	}
-
-	if share.Exists(subPath) {
-		t.Errorf("subpath exists after it shouldnt")
-	}
-
 }
