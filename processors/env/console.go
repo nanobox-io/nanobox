94a903705306786f41d5190c92039485d1f7f2d3
Greg Linton <lintong@pagodabox.com>
2017-08-14 17:06:08 -0600

    Pre-validate console node. Print helpful output
    Resolves #516

--- processors/env/console.go
+++ processors/env/console.go
@@ -5,12 +5,22 @@ import (
 
 	"github.com/nanobox-io/nanobox/models"
 	"github.com/nanobox-io/nanobox/processors/provider"
+	"github.com/nanobox-io/nanobox/util"
 	"github.com/nanobox-io/nanobox/util/console"
 	"github.com/nanobox-io/nanobox/util/display"
 )
 
 // Console ...
 func Console(componentModel *models.Component, consoleConfig console.ConsoleConfig) error {
+	if componentModel.ID == "" {
+		display.ConsoleNodeNotFound()
+		return util.Err{
+			Message: "Node not found",
+			Code:    "USER",
+			Stack:   []string{"failed to console"},
+			Suggest: "It appears the node specified does not exist. Please double check the node name in your boxfile.yml.",
+		}
+	}
 	// setup docker client
 	if err := provider.Init(); err != nil {
 		return err
