8452d1df32ce72053e38aec97e189054ffc89b60
Greg Linton <lintong@pagodabox.com>
2017-09-01 09:28:15 -0600

    Don't pass port so odin uses default for tunnel
    Resolves #563

--- models/tunnel.go
+++ models/tunnel.go
@@ -13,6 +13,6 @@ type (
 		Name  string `json:"name,omitempty"`  // component name being tunneled to
 		Token string `json:"token,omitempty"` // token to complete the tunnel
 		URL   string `json:"url,omitempty"`   // url/ip of nanoagent
-		Port  int    `json:"port"`            // port to tunnel to
+		Port  int    `json:"port,omitempty"`  // port to tunnel to. *must* omitempty. odin will use whatever port is passed (if 0, don't send)
 	}
 )
