f326229f550b3a604250bda3931e28f3c58ac0c1
Greg Linton <lintong@pagodabox.com>
2017-09-14 10:26:34 -0600

    Support password protected ssh keys
    Resolves #575
    Resolves #573

--- generators/hooks/build/user.go
+++ generators/hooks/build/user.go
@@ -1,17 +1,21 @@
 package build
 
 import (
+	"crypto/x509"
 	"encoding/json"
+	"encoding/pem"
+	"fmt"
 	"io/ioutil"
 	"os"
 	"path/filepath"
 	"runtime"
+	"strings"
 
 	"github.com/jcelliott/lumber"
-	"golang.org/x/crypto/ssh"
 
 	"github.com/nanobox-io/nanobox/models"
 	"github.com/nanobox-io/nanobox/util/config"
+	"github.com/nanobox-io/nanobox/util/display"
 )
 
 // UserPayload returns a string for the user hook payload
@@ -66,38 +70,96 @@ func sshKeys() map[string]string {
 
 		// read the contents of the keyfile
 		keyFile := filepath.Join(sshFolder, file.Name())
-		content, err := ioutil.ReadFile(keyFile)
-		// todo: display notice to user failed to read a file
-		if err != nil {
-			lumber.Error("hooks:ioutil.ReadFile(%s): %s", keyFile, err.Error())
-			// if this file cant be read continue on and give as many
-			// of the ssh keys as we can
-			continue
-		}
 
-		// ensure key is not password protected and is a valid private key file
-		_, err = ssh.ParsePrivateKey(content)
+		// validate and return private key
+		keyContents, err := getKey(keyFile)
 		if err != nil {
-			lumber.Error("hooks:ssh.ParsePrivateKey(%s): %s", keyFile, err.Error())
+			lumber.Error("hooks:getKey(%s): %s", keyFile, err.Error())
 			continue
 		}
 
 		// add the keyFile to the list
-		keyFiles[file.Name()] = string(content)
+		keyFiles[file.Name()] = string(keyContents)
 	}
 
 	return keyFiles
 }
 
+// getKey returns a key's bytes for use in fetching dependencies
+func getKey(keyFile string) ([]byte, error) {
+	pemBytes, err := ioutil.ReadFile(keyFile)
+	if err != nil {
+		// display notice to user failed to read a file
+		fmt.Printf("    - Skipping ssh key '%s' (failed to read)\n", keyFile)
+		return nil, fmt.Errorf("hooks:ioutil.ReadFile(%s): %s", keyFile, err.Error())
+	}
+
+	block, _ := pem.Decode(pemBytes)
+	if block == nil {
+		return nil, fmt.Errorf("No key found")
+	}
+	buf := block.Bytes
+
+	if encryptedBlock(block) {
+		if x509.IsEncryptedPEMBlock(block) {
+			// prompt for password to decrypt key
+			fmt.Printf("Password protected key found!\nPlease enter the password for '%s'\n", keyFile)
+			for attempts := 0; attempts < 3; attempts++ {
+
+				passPhrase, err := display.ReadPassword(keyFile)
+				if err != nil {
+					return nil, fmt.Errorf("Failed to read password")
+				}
+				buf, err = x509.DecryptPEMBlock(block, []byte(passPhrase))
+				if err != nil {
+					if err == x509.IncorrectPasswordError {
+						// try again, prompt 3x (mimic `sudo` prompt behavior)
+						if attempts == 2 {
+							fmt.Println("Too many incorrect password attempts, ignoring key.")
+							return nil, err
+						}
+						fmt.Println("Sorry, try again.")
+						continue
+					}
+					// display notice to user failed to parse a file
+					fmt.Printf("    - Skipping ssh key '%s' (failed to decrypt)\n", keyFile)
+					return nil, fmt.Errorf("Cannot decode encrypted private keys: %v", err)
+				} else {
+					break
+				}
+			}
+			block.Headers = map[string]string{}
+		} else {
+			return nil, fmt.Errorf("Key not encrypted PEM block")
+		}
+	}
+
+	block.Bytes = buf
+
+	return pem.EncodeToMemory(block), nil
+}
+
+// encryptedBlock tells whether a private key is
+// encrypted by examining its Proc-Type header
+// for a mention of ENCRYPTED
+// according to RFC 1421 Section 4.6.1.1.
+func encryptedBlock(block *pem.Block) bool {
+	return strings.Contains(block.Headers["Proc-Type"], "ENCRYPTED")
+}
+
 // if there is just one key specified in the config
 // set that as the single key value
-func singleKey(key string) map[string]string {
+func singleKey(keyFile string) map[string]string {
 	keyFiles := map[string]string{}
-	content, err := ioutil.ReadFile(key)
+
+	// validate and return private key
+	keyContents, err := getKey(keyFile)
 	if err != nil {
+		lumber.Error("hooks:getKey(%s): %s", keyFile, err.Error())
 		return keyFiles
 	}
-	keyFiles[filepath.Base(key)] = string(content)
+
+	keyFiles[filepath.Base(keyFile)] = string(keyContents)
 	return keyFiles
 }
 
