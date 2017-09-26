356e3a965c9b5551fa80075e1d226b7ab0dcc2a0
Greg Linton <lintong@pagodabox.com>
2017-09-25 18:40:13 -0600

    Only pull 'nanobox/' images on 'update-images'
    Resolves #584

--- scripts/build.sh
+++ scripts/build.sh
@@ -35,5 +35,7 @@ printf "\nBuilding nanobox updater...\n"
 # change into updater directory and build nanobox updater
 cd ./updater && gox -osarch "darwin/amd64 linux/amd64 windows/amd64" -ldflags="-s" -output="../.build/v2/{{.OS}}/{{.Arch}}/nanobox-update"
 
+cd ..
+
 printf "\nCompacting binaries...\n"
-upx ./.build/v2/darwin/amd64/nanobox-update ./.build/v2/darwin/amd64/nanobox ./.build/v2/windows/amd64/nanobox-update.exe ./.build/v2/windows/amd64/nanobox.exe ./.build/v2/linux/amd64/nanobox-update ./.build/v2/linux/amd64/nanobox
+upx ./.build/v2/*/amd64/nanobox*
